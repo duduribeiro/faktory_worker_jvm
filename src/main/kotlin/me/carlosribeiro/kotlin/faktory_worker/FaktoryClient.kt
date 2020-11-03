f7790dba3ea48bd9ffca95a51fa9970143ce3d76
Cadu Ribeiro <mail@cadu.dev>
2020-11-03 18:25:33 -0300

    Make URI parameter from FaktoryClient as a val

--- src/main/kotlin/me/carlosribeiro/kotlin/faktory_worker/FaktoryClient.kt
+++ src/main/kotlin/me/carlosribeiro/kotlin/faktory_worker/FaktoryClient.kt
@@ -9,9 +9,10 @@ import com.google.gson.Gson
  * @property uri the URI from the Faktory Server. If empty it will use the FAKTORY_URL environment variable and if it
  * is also empty, it will use the localhost.
  */
-class FaktoryClient(uri: String = System.getenv("FAKTORY_URL") ?: "tcp://localhost:7419", internal var connection: FaktoryConnection = FaktoryConnection(uri)) {
-    internal val uri = uri
-
+class FaktoryClient(
+    val uri: String = System.getenv("FAKTORY_URL") ?: "tcp://localhost:7419",
+    internal var connection: FaktoryConnection = FaktoryConnection(uri)
+) {
     fun pushJob(job: FaktoryJob) {
         connection.connect()
         var payload = Gson().toJson(job)
